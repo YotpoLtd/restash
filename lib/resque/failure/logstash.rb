51607abc16b2f57f76dd2e57c422f739f7b6aa1c
Ariel Cabib <ariel.cabib@gmail.com>
2016-06-25 20:13:02 +0300

    adding configurable class. logging to logstash.

--- lib/resque/failure/logstash.rb
+++ lib/resque/failure/logstash.rb
@@ -1,9 +1,22 @@
-require 'logstash-logger'
-require 'resque/failure'
+require 'resque/failure/base'
+require 'restash/conf'
 
 module Resque
   module Failure
     class Logstash < Base
+
+      def save
+        begin
+          Restash::Conf.logger.fatal exception: exception.to_s,
+                                     failure_line: exception.backtrace[0],
+                                     worker: worker.to_s,
+                                     queue: queue,
+                                     payload: payload
+        rescue => e
+          puts "Failed to send to logstash: #{e.message}\n#{e.backtrace}"
+        end
+
+      end
 
     end
   end
---
+++ lib/restash/conf.rb
@@ -0,0 +1,22 @@
+require 'logstash-logger'
+
+module Restash
+  class Conf
+
+    class << self
+
+      attr_accessor :logstash_host, :logstash_port
+
+      def configure
+        yield self
+        true
+      end
+
+      def logger
+        @logger ||= LogStashLogger.new(logstash_host, logstash_port)
+      end
+
+    end
+
+  end
+end
